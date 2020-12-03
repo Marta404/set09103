e94319223d096d9e5e9352c6c9a0bd218727cc72
Marta <ga.marta.web@gmail.com>
2020-12-03 21:51:54 +0000

    login feature wg workbook

--- one.py
+++ one.py
@@ -1,14 +1,82 @@
-from flask import Flask, url_for, request, render_template, redirect
+# from workbook
+import bcrypt
+from functools import wraps
+from flask import Flask
+######################
+from flask import Flask, url_for, request, render_template, redirect, session, flash, jsonify
 # to set up database I'll use SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
+# to create json from database
+from flask_marshmallow import Marshmallow
 # to set up an automatic data in the database when creating a new entry
 from datetime import datetime
+# new to use login
+# , login_user, login_required, logout_user, current_user
+from flask_login import LoginManager, UserMixin
+##from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 # where the database is located, the database is called test.db
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
+#####
+app.secret_key = 'THISisMYsecretKey009'
+valid_email = 'admin@a.com'
+valid_pwhash = bcrypt.hashpw('secretpass'.encode('utf-8'), bcrypt.gensalt())
+
+
+def check_auth(email, password):
+    if (email == valid_email and valid_pwhash == bcrypt.hashpw(password.encode('utf-8'), valid_pwhash)):
+        return True
+    return False
+
+
+def requires_login(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        status = session.get('logged_in', False)
+        if not status:
+            return redirect('/admin/')
+        return f(*args, **kwargs)
+    return decorated
+
+
+@app.route('/admin/logout/')
+def admin_logout():
+    session['logged_in'] = False
+    return 'admin'
+
+
+@app.route('/admin/account/')
+@requires_login
+def account():
+
+    return 'account.html'
+
+
+@app.route('/admin/', methods=['POST', 'GET'])
+def admin():
+    if request.method == 'POST':
+        user = request.form['email1']
+        pw = request.form['pwd']
+
+        if check_auth(user, pw):
+            session['logged_in'] = True
+            return 'logged'
+    return 'not logged'
+
+    ##
+
+
+class Admin(UserMixin, db.Model):
+    # table for admin
+    id = db.Column(db.Integer, primary_key=True)
+    fname = db.Column(db.String(50), nullable=False)
+    sname = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(50), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 # creating a class here - a model
 
 
@@ -21,7 +89,10 @@ class Todo(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
-    # add methods POST and GET to handle data flow to and from the database
+
+class QuestionSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Todo
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -121,23 +192,23 @@ def goodbye():
 # this is account page
 
 
-@app.route("/account/", methods=['GET', 'POST'])
-def account():
-    if request.method == 'POST':
-        print(request.form)
-        name = request.form['name']
-        return "Hello %s" % name
-    else:
-        page = '''
-        <html><body>
-            <form action ="" method="post" name="form">
-                <label for="name">Name:</label>
-                <input type="text" name ="name" id="name"/>
-                <input type="submit" name="submit" id="submit"/>
-            </form>
-        </body></html> '''
+# @app.route("/account/", methods=['GET', 'POST'])
+# def account():
+#     if request.method == 'POST':
+#         print(request.form)
+#         name = request.form['name']
+#         return "Hello %s" % name
+#     else:
+#         page = '''
+#         <html><body>
+#             <form action ="" method="post" name="form">
+#                 <label for="name">Name:</label>
+#                 <input type="text" name ="name" id="name"/>
+#                 <input type="submit" name="submit" id="submit"/>
+#             </form>
+#         </body></html> '''
 
-        return page
+#         return page
 
 
 if __name__ == "__main__":
